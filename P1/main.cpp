909c94cd24667c61555250c6060e8a5315a79419
Bruno Faria <brunofaria1322@gmail.com>
2021-02-18 18:08:04 +0000

    init main

---
+++ P1/main.cpp
@@ -0,0 +1,13 @@
+//Snippet at https://git.dei.uc.pt/snippets/25
+
+#include <iostream>
+
+int main() {
+    // We probably do not need this but it is faster
+    std::ios_base::sync_with_stdio(0);
+    std::cin.tie(0);
+    int n;
+    std::cin >> n;
+    std::cout << n << "\n";
+    return 0;
+}
