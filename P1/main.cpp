dd53e145471d8eeace743fed27595992d150cf35
Bruno Faria <brunofaria1322@gmail.com>
2021-02-21 22:54:43 +0000

    input teading

--- P1/main.cpp
+++ P1/main.cpp
@@ -2,12 +2,40 @@
 
 #include <iostream>
 
+void PrintBoard(int board[20][20], int size){
+    std::cout << "\n";
+
+    for (int j = 0; j<size; j++){
+        for (int k = 0; k<size; k++){
+            std::cout << board[j][k] << " ";
+        }
+        std::cout << "\n";
+    }
+}
+
 int main() {
     // We probably do not need this but it is faster
     std::ios_base::sync_with_stdio(0);
     std::cin.tie(0);
-    int n;
-    std::cin >> n;
-    std::cout << n << "\n";
+
+    int num_tests, board_size, max_moves;
+    std::cin >> num_tests;
+
+
+    for (int i=0; i<num_tests; i++){
+        std::cin >> board_size;
+        std::cin >> max_moves;
+
+        int board[20][20];
+
+        for (int j = 0; j<board_size; j++){
+            for (int k = 0; k<board_size; k++){
+                std::cin >> board[j][k];
+            }
+        }
+
+        PrintBoard(board, board_size);
+    }
+    
     return 0;
 }
