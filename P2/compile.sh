61d1144f71ef51d14b7c9317c3b0f905260b306b
Bruno Faria <brunofaria1322@gmail.com>
2021-04-21 09:20:29 +0100

    added input 3 (output is wrong)

--- P2/compile.sh
+++ P2/compile.sh
@@ -1,8 +1,9 @@
 g++ -std=c++17 -Wall -Wextra -O2 main.cpp -lm
-./a.exe < input.txt > output.txt
 ./a.exe < input1.txt > output1.txt
 ./a.exe < input2.txt > output2.txt
+./a.exe < input3.txt > output3.txt
 
-{ time ./a.exe < input.txt ; } 2> time.txt
+
+{ time ./a.exe < input3.txt ; } 2> time.txt
 
 sleep 5;
